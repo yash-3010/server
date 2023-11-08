201a9ff81b1681b26bc490c7cc55c187a41b25d3
yash-kreeti <yash.barman@kreeti.com>
2023-11-08 06:13:30 +0530

    added env path

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-require('dotenv').config();
+require('dotenv').config({ path: '.env'});
 const app = express();
 
 app.use(express.json());
